f62fe6d13b5da1eac2b0c1fea69050c1ed535dd2
Mojojojooo <yashvernekar674@gmail.com>
2021-09-15 18:33:46 +0530

    basic access

---
+++ Adv/inheritance/access.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+class base{
+    private:
+    // Not accessable by anyone
+    int i=10;
+    protected:
+    // accessable only by inherited
+    int j=20;
+    public:
+    // accessible everywhere
+    int k=30;
+};
+
+class derived : public base {
+    // Public -> public Private -> Private Protected -> Protected
+        public:
+    derived() {
+    cout << "J and K : " << j << " " <<k <<endl;
+
+    }
+    // cout << "J and K : " << j << " " <<k <<endl;
+};
+
+class derived2 : protected base {
+    // Public -> Protected Private -> Private Protected -> Protected
+    // cout << "J and K : " << j << " " <<k <<endl;
+        public:
+    derived2() {
+    cout << "J and K : " << j << " " <<k <<endl;
+
+    }
+};
+
+class derived3 : private base {
+    // Public -> Private Private -> Private Protected -> Private
+    // cout << "No value is accessible " <<endl;
+        public:
+    derived3() {
+    // cout << "J and K : " << j << " " <<k <<endl;
+    cout << "No value is accessible " <<endl;
+    }
+};
+
+int main(){
+    base a;
+    derived b;
+    derived2 c;
+    derived3 d;
+    return 0;
+}
